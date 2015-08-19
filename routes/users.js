b95e5f543331d7f7bd5297af8d450a4982342524
David Zernik <dzernik@gmail.com>
2015-08-19 15:52:00 -0700

    add delete method

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,13 @@ router.get('/new', function(req, res, next) {
   res.render('new.ejs');
 });
 
+
+/* NEW users listing. */
+router.get('/new', function(req, res, next) {
+  res.render('new.ejs');
+});
+
+
 /* EDIT users info. */
 router.get('/edit', function(req, res, next) {
   res.render('edit.ejs');
@@ -20,6 +27,20 @@ router.get('/edit', function(req, res, next) {
 router.get('/delete', function(req, res, next) {
   res.render('../views/delete');
   // res.send('confirm delete page')
+
+router.delete('/delete/confirm', function(req, res){
+  User.findOne({_id: req.body.id}, function(err, user){
+    if(err) {
+      console.log(err);
+      res.json({'message': "error: could not delete user"})
+      return
+    } else {
+      console.log(user);
+      user.remove();
+      res.json({'message': 'deleted user:'+ req.body.id});
+      //res.redirect('/')
+    };
+  });
 });
 
 module.exports = router;
